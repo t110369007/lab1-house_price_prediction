b234a9b74591273b6003b82cf9b483487f33e8a2
t110369007 <93765298+t110369007@users.noreply.github.com>
2021-11-27 21:27:18 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,12 @@
+train.sh
+
+Layer number : 6 層
+Node number for each layer:  64、64*5、64、64*2、64、1
+Loss function: MAE
+Batch Size: 64
+Epoch: 120
+DropOut: 1~2層間 0.3
+Learning Rate: 0.01
+Adaptive: Adam 
+Early Stop: N/A
+Activation: relu, 最後一層 linear
